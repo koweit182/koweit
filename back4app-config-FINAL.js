c7d2d14053591f7d5839da3b80878a0ba8037d27
koweit <koweit23@gmail.com>
2025-12-05 19:40:54 -0500

    Add files via upload

---
+++ back4app-config-FINAL.js
@@ -0,0 +1,10 @@
+const BACK4APP_CONFIG = {
+  applicationId: "vEKWyFWW440jIrgPr1I44YyclHVJcoCjwesu6po7",
+  javascriptKey: "E5vOaCeeDZsOA8PR0SybGoqhtqN4jnyEs1B0tfLZ",
+  serverURL: "https://parseapi.back4app.com"
+};
+
+Parse.initialize(BACK4APP_CONFIG.applicationId, BACK4APP_CONFIG.javascriptKey);
+Parse.serverURL = BACK4APP_CONFIG.serverURL;
+
+console.log("✅ Back4App connecté !");
