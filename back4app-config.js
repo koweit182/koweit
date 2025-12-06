f50c6779d867049dd07c9952b3686b06c46e60c0
koweit <koweit23@gmail.com>
2025-12-05 19:36:45 -0500

    Update back4app-config.js

--- back4app-config.js
+++ back4app-config.js
@@ -2,9 +2,9 @@
 // Remplacez les valeurs ci-dessous par vos vraies clés
 
 const BACK4APP_CONFIG = {
-  applicationId: "vEKWyFWW440jIrgPr1I44YyclHVJcoCjwesu6po7",
-  javascriptKey: "E5vOaCeeDZsOA8PR0SybGoqhtqN4jnyEs1B0tfLZ",
-  serverURL: "https://parseapi.back4app.com"
+  applicationId: 'vEKWyFWW440jIrgPr1I44YyclHVJcoCjwesu6po7',
+  javascriptKey: 'E5vOaCeeDZsOA8PR0SybGoqhtqN4jnyEs1B0tfLZ',
+  serverURL: 'https://parseapi.back4app.com'
 };
 
 // Initialiser Parse
